49aa9507ffdf41bb4f79def841ace28f1c89a95c
Khalil Hadj Messaoud <khadj-me@f1br7s1.clusters.42paris.fr>
2026-01-14 18:48:30 +0100

    module 00: progress, getters study

--- module_00/DivideAndRule.cpp
+++ module_00/DivideAndRule.cpp
@@ -3,8 +3,12 @@
 
 class Bank
 {
+	// Const-macros
+	#define ERROR_NOT_CORRECT_ID "BANK SYSTEM ERROR: account creation failed because new accounts id is negative"
+	#define ERROR_ID_ALREADY_EXISTS "BANK SYSTEM ERROR: account creation failed because new accounts id already exists"
+
 	private:
-		// Account class
+		// Account-class
 		class Account
 		{
 			private:
@@ -24,8 +28,8 @@ class Bank
 				}	
 
 				// Getters/Setters
-				int& const getId() { return this->_id; }
-				int& const getValue() { return this->_value; }
+				const int& getId() { return this->_id; }
+				const int& getValue() { return this->_value; }
 		};
 		// Attributes
 		int _liquidity;
@@ -42,8 +46,6 @@ class Bank
 		// Operator-overloading
 		friend std::ostream& operator << (std::ostream& p_os, const Bank& p_bank)
 		{
-			Account test(1,1);
-			test.getValue() = 2;
 			p_os << "Bank informations : " << std::endl;
 			p_os << "Liquidity : " << p_bank._liquidity << std::endl;
 			for (auto &clientAccount : p_bank._clientAccounts)
@@ -53,37 +55,41 @@ class Bank
 
 		// Getters/Setters
 		const int& getLiquidity() { return this->_liquidity; }
-		const int& const getLiquidity() { return this->_liquidity; }
+		const int& getLiquidity() const { return this->_liquidity; }
 		const std::vector<Account *>& getClientAccounts() { return this->_clientAccounts; }
-		const std::vector<Account *>& const getClientAccounts() { return this->_clientAccounts; }
+		const std::vector<Account *>& getClientAccounts() const { return this->_clientAccounts; }
+
+		// General-methods
+		//  Accounts-manipulation-methods
+		void createAccount(int id, int value)
+		{	
+			// Args-error-checks
+			if (id < 0) {
+				std::cout << ERROR_NOT_CORRECT_ID << std::endl;
+				return;
+			}
+
+			std::vector<Account *>::iterator it_begin = _clientAccounts.begin();
+			std::vector<Account *>::iterator it_end = _clientAccounts.end();
+			for (std::vector<Account *>::iterator it = it_begin; it < it_end; it++)
+				if ((*it)->getId() == id) {
+					std::cout << ERROR_ID_ALREADY_EXISTS << std::endl;
+					return;
+				}
+			Account newAccount(id, value);
+			this->_clientAccounts.push_back(&newAccount);
+		}
 };
 
 int main()
 {
-	Account accountA = Account();
-	accountA.id = 0;
-	accountA.value = 100;
+	Bank bank;
 
-	Account accountB = Account();
-	accountB.id = 1;
-	accountB.value = 100;
-
-	Bank bank = Bank();
-	bank.liquidity = 999;
-	bank.clientAccounts.push_back(&accountA);
-	bank.clientAccounts.push_back(&accountB);
-
-	bank.liquidity -= 200;
-	accountA.value += 400;
-
-	std::cout << "Account : " << std::endl;
-	std::cout << accountA << std::endl;
-	std::cout << accountB << std::endl;
-
-	std::cout << " ----- " << std::endl;
-
-	std::cout << "Bank : " << std::endl;
-	std::cout << bank << std::endl;
+	// Account-creation-tests
+	bank.createAccount(-2, 100);
+	bank.createAccount(0, 100);
+	bank.createAccount(0, 100);
+	bank.createAccount(0, 100);
 
 	return (0);
 }
--- module_00/RoadMap.md
+++ module_00/RoadMap.md
@@ -3,8 +3,11 @@ Build a correct bank and accounts system with correct and logic encapsulation
 Requirements:
 - [ ] The bank must receive 5% of each money inflow on these client accounts
 - [ ] The accounts must never have two identical IDs
-- [ ] The attributes of the structures must not be modifiable from the outside
+- [x] The attributes of the structures must not be modifiable from the outside
 - [ ] The bank must be able to create, delete and modify the accounts of these customers
+ - [ ] create
+ - [ ] delete
+ - [ ] modify
 - [ ] The bank must be able to give a loan to a customer, within the limits of its funds
 - [ ] It must be impossible to add money to a client account without going through the bank
 - [x] If it makes sense, the creation of a Getter and a Setter is mandatory. Getter(s) by copy will not be accepted
